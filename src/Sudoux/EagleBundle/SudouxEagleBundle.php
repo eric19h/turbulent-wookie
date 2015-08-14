7b54b1687e9e58536cf183e88f55ae62ce157265
Eric Haynes <eric@webmaxco.com>
2015-08-14 10:19:57 -0400

    Added override routes for Eagle Bundle

--- src/Sudoux/EagleBundle/SudouxEagleBundle.php
+++ src/Sudoux/EagleBundle/SudouxEagleBundle.php
@@ -4,6 +4,24 @@ namespace Sudoux\EagleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+
+/**
+ * Class SudouxEagleBundle
+ * @package Sudoux\EagleBundle
+ * @author Eric Haynes
+ */
 class SudouxEagleBundle extends Bundle
 {
+
+    /**
+     * @author Eric Haynes
+     */
+    public function getParent(){
+
+        return 'SudouxMortgageBundle';
+
+    }
+
+
+
 }
